44132d8a637ae739f779f07377b40ab2882fe961
Wolfgang Klinger <wolfgang@wazum.com>
2026-08-27 05:55:04 +0200

    Add the no-translate command and its span output

--- src/no-translate.ts
+++ src/no-translate.ts
@@ -1,7 +1,23 @@
 import { Plugin } from '@ckeditor/ckeditor5-core';
+import { AttributeCommand } from '@ckeditor/ckeditor5-basic-styles';
+
+const ATTRIBUTE = 'noTranslate';
 
 export class NoTranslate extends Plugin {
     public static get pluginName() {
         return 'NoTranslate' as const;
+    }
+
+    public init(): void {
+        const editor = this.editor;
+
+        editor.model.schema.extend('$text', { allowAttributes: ATTRIBUTE });
+
+        editor.conversion.for('downcast').attributeToElement({
+            model: ATTRIBUTE,
+            view: { name: 'span', attributes: { translate: 'no' } },
+        });
+
+        editor.commands.add(ATTRIBUTE, new AttributeCommand(editor, ATTRIBUTE));
     }
 }
